8fb614b37fcc9946b10f970ae6ca19c732a4674b
Pranay Desai <pranayxdesai@gmail.com>
2021-01-11 15:48:56 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
+import BottomSheet from './src/BottomSheet';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <BottomSheet />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
