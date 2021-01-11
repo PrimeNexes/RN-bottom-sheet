a26970fd82564b9105863f1da86df35b1756173a
Pranay Desai <pranayxdesai@gmail.com>
2021-01-11 16:10:06 +0530

    test

--- src/BottomSheet.js
+++ src/BottomSheet.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
-import {FlingGestureHandler,Directions,State} from 'react-native-gesture-handler';
+import {View, Alert} from 'react-native';
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from 'react-native-gesture-handler';
 
 function BottomSheet() {
   return (
-   
-       <FlingGestureHandler
-        direction={Directions.RIGHT}
-        onHandlerStateChange={({ nativeEvent }) => {
-          Alert.alert("I'm flinged!");
-          if (nativeEvent.state === State.ACTIVE) {
-          
-          }
-        }}>
-        <View style={{backgroundColor:'grey',width:100,height:100}} />
-      </FlingGestureHandler>
+    <FlingGestureHandler
+      direction={Directions.RIGHT}
+      onHandlerStateChange={({nativeEvent}) => {
+        Alert.alert("I'm flinged!");
+        if (nativeEvent.state === State.ACTIVE) {
+        }
+      }}>
+      <View style={{backgroundColor: 'grey', width: 100, height: 100}} />
+    </FlingGestureHandler>
   );
 }
 
