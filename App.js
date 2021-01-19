4a522ff7b8ae91b42264989270a4142a4fb3c517
Pranay Desai <pranayxdesai@gmail.com>
2021-01-19 14:28:15 +0530

    v0.1

--- App.js
+++ App.js
@@ -7,7 +7,14 @@
  */
 
 import React from 'react';
-import {SafeAreaView, View, StatusBar, Button, Alert} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StatusBar,
+  Button,
+  Alert,
+  StyleSheet,
+} from 'react-native';
 import BottomSheet from './src/BottomSheet';
 
 const App = () => {
@@ -15,26 +22,47 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-      <View style={{height:'100%',width:'100%',backgroundColor:'black'}}>
-          <Button title={"PRESS ME"} onPress={()=>{Alert.alert("Doing Main")}}></Button>
+        <View style={styles.content}>
+          <Button
+            title={'PRESS ME'}
+            onPress={() => {
+              Alert.alert('Doing Main');
+            }}
+          />
         </View>
-        <BottomSheet style={{backgroundColor:'white', borderTopLeftRadius:6,borderTopRightRadius:6}} 
-        header={ <View style={{
-          marginTop:10,
-          marginBottom:14,
-          height: 4,
-          borderRadius: 4,
-          backgroundColor: '#CBCDCC88',
-          alignSelf:'center',
-          width: 36,
-        }}/>}>
-        <View style={{flexDirection:'column',backgroundColor:'grey'}}>
-          <Button title={"PRESS ME"} onPress={()=>{Alert.alert("Doing Sheet")}} />
-        </View>
+        <BottomSheet
+          style={styles.bottomSheet}
+          header={<View style={styles.bottomSheetHeader} />}>
+          <View style={styles.bottomSheetContent}>
+            <Button
+              title={'PRESS ME'}
+              onPress={() => {
+                Alert.alert('Doing Sheet');
+              }}
+            />
+          </View>
         </BottomSheet>
       </SafeAreaView>
     </>
   );
 };
 
+const styles = StyleSheet.create({
+  content: {height: '100%', width: '100%', backgroundColor: 'black'},
+  bottomSheet: {
+    backgroundColor: 'white',
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+  },
+  bottomSheetHeader: {
+    marginTop: 10,
+    marginBottom: 14,
+    height: 4,
+    borderRadius: 4,
+    backgroundColor: '#CBCDCC88',
+    alignSelf: 'center',
+    width: 36,
+  },
+  bottomSheetContent: {flexDirection: 'column', backgroundColor: 'grey'},
+});
 export default App;
--- src/BottomSheet.js
+++ src/BottomSheet.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {View, Text, StyleSheet, SafeAreaView, useWindowDimensions} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import {
   FlingGestureHandler,
   Directions,
@@ -14,13 +14,13 @@ import Animated, {
 } from 'react-native-reanimated';
 
 function BottomSheet(props) {
-  
   const offset = useSharedValue(0);
-  const defaultHeight = props.defaultHeight || 20 ;
-  const height = props.height || (useWindowDimensions().height/2);
+  const windowHeight = useWindowDimensions().height;
+  const defaultHeight = props.defaultHeight || 20;
+  const height = props.height || windowHeight / 2;
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      height: withSpring( offset.value + defaultHeight ),
+      height: withSpring(offset.value + defaultHeight),
     };
   });
 
@@ -29,26 +29,24 @@ function BottomSheet(props) {
       <FlingGestureHandler
         direction={Directions.UP}
         onHandlerStateChange={({nativeEvent}) => {
-          if (nativeEvent.state === State.ACTIVE && (offset.value !== height)) {
+          if (nativeEvent.state === State.ACTIVE && offset.value !== height) {
             offset.value = offset.value + height;
           }
         }}>
-          <FlingGestureHandler
-        direction={ Directions.DOWN}
-        onHandlerStateChange={({nativeEvent}) => {
-          if (nativeEvent.state === State.ACTIVE && (offset.value === height)) {
-            offset.value = offset.value - height;
-          }
-        }}>
-        <Animated.View style={[styles.modalBox,animatedStyles,props.style]}>
+        <FlingGestureHandler
+          direction={Directions.DOWN}
+          onHandlerStateChange={({nativeEvent}) => {
+            if (nativeEvent.state === State.ACTIVE && offset.value === height) {
+              offset.value = offset.value - height;
+            }
+          }}>
+          <Animated.View style={[styles.modalBox, animatedStyles, props.style]}>
             {props.header}
-          <ScrollView style={styles.content}>
-            {props.children}
-          </ScrollView>
-        </Animated.View>
+            <ScrollView style={styles.content}>{props.children}</ScrollView>
+          </Animated.View>
         </FlingGestureHandler>
       </FlingGestureHandler>
-      </View>
+    </View>
   );
 }
 
@@ -58,16 +56,14 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
   modalBox: {
-    width:'100%',
+    width: '100%',
   },
-  content :
-  {
-    top:25,
-    height:'100%',
-    width:'100%',
-    position:'absolute',
-  }
-})
-
+  content: {
+    top: 25,
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+  },
+});
 
 export default BottomSheet;
